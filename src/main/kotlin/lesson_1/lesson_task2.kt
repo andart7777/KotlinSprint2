386c103a16e97b8b66ceb12f0830f39a5ca097f1
Andrew Z <andart7777@yandex.ru>
2024-08-16 16:23:48 +0600

    Print variables and Number of employees

---
+++ src/main/kotlin/lesson_1/lesson_task2.kt
@@ -0,0 +1,19 @@
+package lesson_1
+
+fun main() {
+    var numberOfStoreOrders: Int = 75 // "var" - количество заказов будет изменяться?
+    val textWithThanksForYourPurchase: String = "Спасибо за покупку!"
+
+    println(numberOfStoreOrders)
+    println(textWithThanksForYourPurchase)
+
+
+    var numberOfEmployees: Int = 2_000
+
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1_999
+
+    println(numberOfEmployees)
+}
+
