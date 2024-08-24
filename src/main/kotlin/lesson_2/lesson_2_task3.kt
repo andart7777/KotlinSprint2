d7a24e20b7f2fee9763ba09a7e538a241af761a9
Andrew Z <andart7777@yandex.ru>
2024-08-24 21:13:17 +0600

    KS-2-3 Task train arrival time

---
+++ src/main/kotlin/lesson_2/lesson_2_task3.kt
@@ -0,0 +1,15 @@
+package lesson_2
+
+fun main() {
+
+    var trainDepartureTimeHour: Int = 9
+    var trainDepartureTimeMinute: Int = 39
+    var trainTravelTime: Int = 457
+
+    var fullTrainArrivalTimeInMinute: Int = (trainDepartureTimeHour * 60) + trainDepartureTimeMinute + trainTravelTime
+
+    var arrivalHour: Int = fullTrainArrivalTimeInMinute / 60
+    var arrivalMinute: Int = fullTrainArrivalTimeInMinute % 60
+
+    println("Время прибытия поезда: $arrivalHour:$arrivalMinute")
+}
